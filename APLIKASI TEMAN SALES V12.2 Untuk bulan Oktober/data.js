69310879628d638f2a905a67e0d85520a6706060
Riqqie <ikyu.ahmad@gmail.com>
2025-10-28 16:25:19 +0700

    Update data.js

--- APLIKASI TEMAN SALES V12.2 Untuk bulan Oktober/data.js
+++ APLIKASI TEMAN SALES V12.2 Untuk bulan Oktober/data.js
@@ -70,14 +70,6 @@ const config = {
         { id: "SKU064", group: "KRISBITE", name: "KRISBITE COKLAT", price: 133600, boxPerCarton: 8 },
         { id: "SKU065", group: "GRANDE", name: "GRANDE COKLAT", price: 100200, boxPerCarton: 12 },
         { id: "SKU066", group: "GRANDE", name: "GRANDE CHEESECAKE STRAWBERRY", price: 100200, boxPerCarton: 6 },
-/*
-        { id: "SKU067", group: "NPL", name: "BDAY COOKIE RICHBUTTER", price: 100000, boxPerCarton: 6 },
-        { id: "SKU068", group: "NPL", name: "WAFFLE FRIES FIRE WINGS", price: 99000, boxPerCarton: 60 },
-        { id: "SKU069", group: "NPL", name: "WAFFLE FRIES CHEESEBURGER", price: 99000, boxPerCarton: 60 },
-        { id: "SKU054", group: "NPL", name: "WRAPZ KLAPA", price: 122400, boxPerCarton: 12 },
-        { id: "SKU055", group: "NPL", name: "WRAPZ COKELAT", price: 122400, boxPerCarton: 12 },
-        { id: "SKU056", group: "NPL", name: "WRAPZ GOGUMA", price: 122400, boxPerCarton: 12 }, 
-        { id: "SKU077", group: "CHOCOPIE", name: "CHOCOPIE COKELAT", price: 159360, boxPerCarton: 8 }, */
 		{ id: "SKU078", group: "CSD 2K 12 PCS", name: "AMO CSD 2K LEMON", price: 20160, boxPerCarton: 12 },
         { id: "SKU079", group: "CSD 2K 12 PCS", name: "AMO CSD 2K COLA", price: 20160, boxPerCarton: 12 },
         { id: "SKU080", group: "CSD 2K 12 PCS", name: "AMO CSD 2K STRAWBERRY", price: 20160, boxPerCarton: 12 },
@@ -108,11 +100,11 @@ const config = {
         strataGrouping: {
 			"WAFER 500": [ { minQty: 10, potongan: 3500 }, { minQty: 6, potongan: 3000 }, { minQty: 3, potongan: 2750 }, { minQty: 1, potongan: 2300 } ],
 			"WAFER 1K": [ { minQty: 10, potongan: 3500 }, { minQty: 6, potongan: 3000 }, { minQty: 3, potongan: 2750 }, { minQty: 1, potongan: 2300 } ],
-			"ROLL 500": [ { minQty: 10, potongan: 3500 }, { minQty: 6, potongan: 3000 }, { minQty: 3, potongan: 2750 }, { minQty: 1, potongan: 2300 } ],
-			"ROLL 1K": [ { minQty: 10, potongan: 3500 }, { minQty: 6, potongan: 3000 }, { minQty: 3, potongan: 2750 }, { minQty: 1, potongan: 2300 } ],
+			"ROLLS 500": [ { minQty: 10, potongan: 3500 }, { minQty: 6, potongan: 3000 }, { minQty: 3, potongan: 2750 }, { minQty: 1, potongan: 2300 } ],
+			"ROLLS 1K": [ { minQty: 10, potongan: 3500 }, { minQty: 6, potongan: 3000 }, { minQty: 3, potongan: 2750 }, { minQty: 1, potongan: 2300 } ],
 			"TIMEBREAK 1K": [ { minQty: 10, potongan: 3500 }, { minQty: 6, potongan: 3000 }, { minQty: 3, potongan: 2750 }, { minQty: 1, potongan: 2300 } ],
 			"TIMEBREAK 2K": [ { minQty: 10, potongan: 3500 }, { minQty: 6, potongan: 3000 }, { minQty: 3, potongan: 2750 }, { minQty: 1, potongan: 2300 } ],
-			"PASTA 500": [ { minQty: 10, potongan: 3500 }, { minQty: 6, potongan: 3000 }, { minQty: 3, potongan: 2750 }, { minQty: 1, potongan: 2300 } ],
+			"PASTA": [ { minQty: 10, potongan: 3500 }, { minQty: 6, potongan: 3000 }, { minQty: 3, potongan: 2750 }, { minQty: 1, potongan: 2300 } ],
 			"WAFER 2K": [ { minQty: 300, potongan: 5300 }, { minQty: 100, potongan: 5000 }, { minQty: 50, potongan: 4750 }, { minQty: 25, potongan: 4250 }, { minQty: 10, potongan: 3500 },  { minQty: 6, potongan: 3000 }, { minQty: 3, potongan: 2750 }, { minQty: 1, potongan: 2300 } ],
 			"WAFER 5K": [ { minQty: 300, potongan: 7600 }, { minQty: 100, potongan: 7300 }, { minQty: 50, potongan: 7050 }, { minQty: 25, potongan: 6550 }, { minQty: 10, potongan: 5800 },  { minQty: 6, potongan: 5300 }, { minQty: 3, potongan: 5050 }, { minQty: 1, potongan: 2300 } ],
 			"AHH 500": [ { minQty: 10, potongan: 3500 }, { minQty: 6, potongan: 3000 }, { minQty: 3, potongan: 2750 }, { minQty: 1, potongan: 2300 } ],
@@ -130,12 +122,12 @@ const config = {
         },
         strataPengambilan: {
             "main": {
-                "appliesTo": ["WAFER 500", "WAFER 1K", "ROLL 500", "ROLL 1K", "TIMEBREAK 1K", "TIMEBREAK 2K", "PASTA 500", "WAFER 2K", "WAFER 5K", "AHH 500", "AHH 1K", "SIIP 500", "SIIP 1K", "SIIP 2K", "NEXTAR 2K", "CSD 2K 12 PCS", "CSD 2K 24 PCS", "HELLO PANDA" ],
+                "appliesTo": ["WAFER 500", "WAFER 1K", "ROLLS 500", "ROLLS 1K", "TIMEBREAK 1K", "TIMEBREAK 2K", "PASTA", "WAFER 2K", "WAFER 5K", "AHH 500", "AHH 1K", "SIIP 500", "SIIP 1K", "SIIP 2K", "NEXTAR 2K", "CSD 2K 12 PCS", "CSD 2K 24 PCS", "HELLO PANDA",  "KRIMERO", "NEXTAR STAR", "NEXTAR NOIR", "NEXTAR 5K", "KRISBITE", "GRANDE" ],
                 "tiers": [ { "minValue": 3000000, "discount": 0.02 }, { "minValue": 2000000, "discount": 0.015 }, { "minValue": 1000000, "discount": 0.01 } ]
             },
             "simba": {
                 // <-- DITAMBAHKAN: "SIMBA BULKY" dimasukkan ke strata pengambilan Simba
-                "appliesTo": ["SIMBA", "SIMBA 2 in 1", "SIMBA BULKY"],
+                "appliesTo": ["SIMBA 500", "SIMBA 1K", "SIMBA 2K", "SIMBA 2 in 1", "SIMBA BULKY"],
                 "tiers": [ { "minValue": 3000000, "discount": 0.03 }, { "minValue": 2000000, "discount": 0.025 }, { "minValue": 1000000, "discount": 0.015 }, { "minValue": 500000,  "discount": 0.0125 }, { "minValue": 300000,  "discount": 0.01 } ]
             }
         },
@@ -200,6 +192,7 @@ const config = {
     ]
 
 };
+
 
 
 
